231d7c0956fe75bd1920b6e1e1a459a1dcc939f1
Akshat <apodduto@purdue.edu>
2018-11-14 22:10:55 -0500

    Created skeleton of ChatMessage

--- src/ChatMessage.java
+++ src/ChatMessage.java
@@ -1,8 +1,55 @@
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 final class ChatMessage implements Serializable {
     private static final long serialVersionUID = 6898543889087L;
 
     // Here is where you should implement the chat message object.
     // Variables, Constructors, Methods, etc.
+
+    private String type;
+    private String message;
+
+    public ChatMessage (int type, String message) {
+        if (type == 0) {
+            this.type = "general";
+        } else if (type == 1) {
+            this.type = "logout";
+        }
+        this.message = message;
+    }
+
+
+
+    private void broadcast(String message) {
+
+    }
+
+    private boolean writeMessage(String msg) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        System.out.println(formatter.format(date));
+
+
+        return true;
+    }
+
+    private void remove(int id) {
+
+    }
+
+    public void run() {
+
+    }
+
+    private void close() {
+
+    }
+
+
+
+
+
+
 }
